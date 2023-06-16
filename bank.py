6b9667f34e27407de14304fb2ee197a02c7a25c5
JuanMaDev <86654681+0MFCAT@users.noreply.github.com>
2023-06-15 23:46:06 -0400

    added some new IDE files (PyCharm)

--- bank.py
+++ bank.py
@@ -4,21 +4,20 @@ import database as db
 
 
 class User:
-
     all_users = []
 
     def __init__(self, first_name: str, last_name: str, year_of_birth: int, country: str, email: str, password: str):
 
         assert len(first_name) < 25, "String length exceeded, max length is 25"
         assert len(last_name) < 25, "String length exceeded, max length is 25"
-        assert year_of_birth > 0 and year_of_birth < date.today().year, "Must input a valid year"
-        #assert country #TODO: make the attribute country check wether your country initials exists or not and is supported, using a dictionary
-        #assert email #TODO: check if email is valid and is not already taken
-        #assert password #TODO: Check if password length is good enough, and if it has the requisites like special characters, uppers and lowers, etc...
+        assert 0 < year_of_birth < date.today().year, "Must input a valid year"
+        # assert country #TODO: make the attribute country check wether your country initials exists or not and is supported, using a dictionary
+        # assert email #TODO: check if email is valid and is not already taken
+        # assert password #TODO: Check if password length is good enough, and if it has the requisites like special characters, uppers and lowers, etc...
 
         self.first_name = first_name
         self.last_name = last_name
-        self._full_name = f"{first_name} {last_name}" #Property TODO: chequear por que esto da error si le quito la _
+        self._full_name = f"{first_name} {last_name}"  # Property TODO: chequear por que esto da error si le quito la _
         self.year_of_birth = year_of_birth
         self._age = date.today().year - year_of_birth
         self.country = country
@@ -26,9 +25,9 @@ class User:
         self._password = password
         self.is_logged = False
 
-        #Actions
+        # Actions
 
-        User.all_users.append(self) # Creates a list with all user objects, must be handy to something I guess
+        User.all_users.append(self)  # Creates a list with all user objects, must be handy to something I guess
 
     @property
     def full_name(self):
@@ -44,7 +43,7 @@ class User:
         self.first_name = first
         self.last_name = " ".join(last)
 
-    #TODO: Create a Constructor Method to initialize the Users from a CSV file, later migrate it to a SQL database
+    # TODO: Create a Constructor Method to initialize the Users from a CSV file, later migrate it to a SQL database
 
     def __repr__(self):
         return f'{self.__class__.__name__}("{self.first_name}", "{self.last_name}", {self.year_of_birth}, "{self.country}", "{self.email}", "{self._password}")'
@@ -58,27 +57,27 @@ class User:
 
     @staticmethod
     def signing(email: str, password: str):
-        #TODO: Make the logic to create a new user in the database
+        # TODO: Make the logic to create a new user in the database
         return True
 
 
 class Admin(User):
-
     all_admins = []
 
     def __init__(self, first_name: str, last_name: str, year_of_birth: int, country: str, email: str, password: str):
         super().__init__(first_name, last_name, year_of_birth, country, email, password)
 
-        #Actions
+        # Actions
         Admin.all_admins.append(self)
 
 
-class Bank_account: #Uses a User object and assign him an ID to make bank transactions
+class Bank_account:  # Uses a User object and assign him an ID to make bank transactions
 
     @staticmethod
     def generate_id():
-        #TODO: check wether the ID is in use or not
-        return randint(100000000, 999999999) #gives the bank account a random ID, capacity for 8.999.999.999 users, expandible if necessary
+        # TODO: check wether the ID is in use or not
+        return randint(100000000,
+                       999999999)  # gives the bank account a random ID, capacity for 8.999.999.999 users, expandible if necessary
 
     def __init__(self, bank_user: User):
         self.bank_user = bank_user
@@ -89,9 +88,7 @@ class Bank_account: #Uses a User object and assign him an ID to make bank transa
         return self._bank_ID
 
 
-
 def main():
-
     Juan = Admin("Juan Manuel", "González Vega", 1998, "CUB", "juanmgv98@gmail.com", "QWERTY")
     Dorian = User("Dorian", "Gay Perez", 1998, "CUB", "dorianperez@gmail.com", "12345")
     Alejandro = User("Alejandro", "Marrero Garcia", 1997, "CUB", "manko@gmail.com", "axax")
@@ -107,12 +104,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
